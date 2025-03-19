9f0e45e67bd1392f5165b64cbdfe46fecfe87fb0
umroot <umroot@veteran.encs.concordia.ca>
2025-03-19 12:45:18 -0400

    file reformating

--- ContactOPedia/contactopedia/src/Components/Layout/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="text-center" style={{ color: "white", fontSize: "50px" }}>
-      Happy Coding! @ContactOPedia
-    </div>
-  );
-}
